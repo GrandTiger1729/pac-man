6301eb500d40813390cd7b6b3b7746ec349d6cef
GrandTiger1729 <80200287+GrandTiger1729@users.noreply.github.com>
2023-11-26 19:17:31 +0800

    Initial Commitment

---
+++ core/pacman.py
@@ -0,0 +1,9 @@
+import pygame
+
+class PacMan(pygame.sprite.Sprite):
+    """The pac-man
+    """
+
+    def __init__(self):
+        pass
+
---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+running = True
+
+def handle_event():
+    pass
+
+def update():
+    pass
+
+while running:
+
+    handle_event()
+
+    update()
